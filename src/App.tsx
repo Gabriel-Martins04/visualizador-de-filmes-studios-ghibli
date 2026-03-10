1772d35182352df971a6b24a4076799b1e59042a
Gabriel Martins <dab.martins2004@gmail.com>
2026-03-10 20:20:28 -0300

    feat: adicionando mais configurações para realizar o deploy

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { Router } from "./routes";
 function App() {
 	return (
 		<>
-			<BrowserRouter>
+			<BrowserRouter basename="/visualizador-de-filmes-studios-ghibli">
 				<Router />
 			</BrowserRouter>
 		</>
